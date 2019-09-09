e4daa6fd0ab8de03c2e232ac0f51ccd82eae8a51
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-09-09 19:44:28 +0300

    Add create action (for POST /articles)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     render json: @articles, status: :ok
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      render json: @article, status: :created
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :image_url)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { within: 2..100 }
 end
